b401a98aed1723224e6508140c6bac5e039d035d
machntoshh <erimgomess@gmail.com>
2025-03-25 19:19:43 -0300

    New Commit (Laço de repetição)

---
+++ src/Condicoes/lacoDeRepeticao.java
@@ -0,0 +1,25 @@
+package Condicoes;
+
+public class lacoDeRepeticao {
+    public static void main(String[] args) {
+        /*
+        *   Vão repetir infitamente ou até atingir o param desejado
+        *   (while)
+        *   (for)
+        * */
+
+        int shadowNumbers = 0;
+        int maxShadowNumbers = 30;
+
+        // while
+        while (shadowNumbers < 10) {
+            shadowNumbers++; // número de shadowNumbers + 1 enquanto n chegar a 10
+            System.out.println("Jin woo chamou " + shadowNumbers + " sombras");
+        }
+
+        // for
+        for (int i = 0; i <= maxShadowNumbers ; i++) {
+            System.out.println("Jin woo chamou " + i + " sombras");
+        }
+    }
+}
---
+++ src/Condicoes/ternaryOps.java
@@ -0,0 +1,16 @@
+package Condicoes;
+
+public class ternaryOps {
+    public static void main(String[] args) {
+        /*
+        *   Maneira de reduzir o código
+        *   variável = (condição) ? valorTrue : valorFalse | pegou a ideia?
+        *   if <- ?
+        *   else <- :
+        * */
+
+        Short numMissoes = 11;
+        String nivel = (numMissoes >= 10) ? "Esse hunter tem mais de 10 missões" : "Esse hunter tem menos de 10 missões";
+        System.out.println(nivel);
+    }
+}
