95a2e94b0138a59c90aefd2e217d2eab37fba0dd
machntoshh <erimgomess@gmail.com>
2025-03-27 09:21:41 -0300

    New Commit (array, novos conhecimentos)

--- src/BasicLevel/DataStructures/arrayInJava.java
+++ src/BasicLevel/DataStructures/arrayInJava.java
@@ -3,18 +3,31 @@ package BasicLevel.DataStructures;
 public class arrayInJava {
     public static void main(String[] args) {
 
+        // arrays e listas são diferentes
+        // arrays sempre tem tipos definidos e listas são flexiveis
+        // arrays são tipo referência
+
         String[] hunters = new String[3]; // esse array vai receber só 3 entradas
         hunters[0] = "Sun Jin Woo";
         hunters[1] = "Cha Hae Ji";
         hunters[2] = "Go";
-
         System.out.println(hunters[2]);
+        System.out.println(hunters); // estou pedindo, perguntando qual o tipo de referência de memória
 
-        // Erro [Ljava.lang.String;@3fee733d o que significa? <- Desafio abacaxi
+
+        // tipos diferentes de array, dão respostas diferentes
 
 
-        /*String hunter1 = "Jin Woo";
-        String hunter2 = "Cha Hae Ji";
-        String hunter3 = "Baek Yoonho";*/
+        int[] idade = new int[2]; // new int, novo objeto de memória com referência de 2 campos
+        idade[0] = 22;
+        idade[1] = 11;
+        System.out.println(idade[1]);
+        System.out.println(idade);
+
+
+        boolean[] realOuFeiki = new boolean[2];
+        // que tipo de memória vai retornar?
+        System.out.println(realOuFeiki[0]);
+
     }
 }
