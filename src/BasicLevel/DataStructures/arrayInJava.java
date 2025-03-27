acae911a1d1d40ced46b9dca235b3a42a45b7021
machntoshh <erimgomess@gmail.com>
2025-03-27 09:49:10 -0300

    New Commit (array, alocação de memo, override de array e etc)

--- src/BasicLevel/DataStructures/arrayInJava.java
+++ src/BasicLevel/DataStructures/arrayInJava.java
@@ -7,10 +7,15 @@ public class arrayInJava {
         // arrays sempre tem tipos definidos e listas são flexiveis
         // arrays são tipo referência
 
-        String[] hunters = new String[3]; // esse array vai receber só 3 entradas
+        String[] hunters = new String[8]; // esse array vai receber só 3 entradas
         hunters[0] = "Sun Jin Woo";
         hunters[1] = "Cha Hae Ji";
-        hunters[2] = "Go";
+        hunters[2] = "Go Gunhee";
+        hunters[3] = "Goto Ryuji";
+        hunters[4] = "Choi Jong In";
+        hunters[5] = "Ma Dongwook";
+        hunters[6] = "Kanae Tawata";
+        hunters[7] = "Sung Il Hwan";
         System.out.println(hunters[2]);
         System.out.println(hunters); // estou pedindo, perguntando qual o tipo de referência de memória
 
@@ -24,10 +29,20 @@ public class arrayInJava {
         System.out.println(idade[1]);
         System.out.println(idade);
 
+        // se tentar redeclarar um array, ele vai sobrescrever toda a memória utilizada
+        idade = new int[2];
+        System.out.println(idade[0]); // olha oq retorna
+
 
         boolean[] realOuFeiki = new boolean[2];
         // que tipo de memória vai retornar?
         System.out.println(realOuFeiki[0]);
+
+
+        // loop em array
+        for (int i = 0; i < 8; i++) {
+            System.out.println(hunters[i]);
+        }
 
     }
 }
