12a55604a9e070ce20121b37f486f67acb957c1a
machntoshh <erimgomess@gmail.com>
2025-03-31 18:22:03 -0300

    New Commit (classes e métodos)

--- src/IntermediateLevel/Main.java
+++ src/IntermediateLevel/Main.java
@@ -3,17 +3,33 @@ package IntermediateLevel;
 public class Main {
     public static void main(String[] args) {
         // criar uma pessoa - meu mano pessoa é um objeto
-
+        // Objeto Eric Marques
         Pessoa Eric = new Pessoa(); // inicializei um obj usando o molde de classe Pessoa
         Eric.nome = "Eric Marques";
         Eric.idade = 22;
         Eric.ikigai = "Programador Java";
         System.out.println("Nome é " + Eric.nome + ", sua idade é " + Eric.idade + " e seu ikigai é o de " + Eric.ikigai);
 
+        // Objeto Miguel Marques
         Pessoa Miguel = new Pessoa();
         Miguel.nome = "Miguel Marques";
         Miguel.idade = 11;
         Miguel.ikigai = "Brincar";
         System.out.println("Nome é " + Miguel.nome + ", sua idade é " + Miguel.idade + " e seu ikigai é o de " + Miguel.ikigai);
+
+        String miguelFalandoBulhufas = Miguel.textoRandom();
+        System.out.println(miguelFalandoBulhufas);
+
+        // Objeto Sasuke Uchiha
+        Pessoa Sasuke = new Pessoa();
+        Sasuke.nome = "Sasuke Uchiha";
+        Sasuke.idade = 16;
+        Sasuke.ikigai = "Ninja de Konoha";
+        System.out.println(Sasuke.nome + " vai ativar seu Doujutsu");
+        // aplicando metodo ao meu objeto
+        Sasuke.sharinganAtivado();
+        // aplicando metodo idade no Sasuke
+        int idadeDezoito = Sasuke.quantoPraDezoito(18);
+        System.out.println("Falta " + idadeDezoito + " anos para " + Sasuke.nome + " ter 18");
     }
 }
--- src/IntermediateLevel/Pessoa.java
+++ src/IntermediateLevel/Pessoa.java
@@ -4,4 +4,26 @@ public class Pessoa { // molde de classe, básicamente
     String nome;
     int idade;
     String ikigai;
+
+    // Criando um mét0do público personalizado
+    /*
+    *  void n retorna valor nenhum
+    * */
+    public void sharinganAtivado() {
+        System.out.println("Sharingan!!!");
+    }
+
+    /*
+    *   Metodo string vai retornar uma string
+    * */
+    public String textoRandom() {
+        return "ablubluble";
+    }
+
+    /*
+     *   Metodo int vai retornar um int
+     * */
+    public int quantoPraDezoito(int idadeRequerida) {
+        return idadeRequerida - idade;
+    }
 }
